3cd5b1844a6f06090c89d53a7e3ea0157d8141e2
mohammed raazy <muhammadrazi51@gmail.com>
2022-05-30 19:49:22 +0700

    still working on navbar responsive

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -11,45 +11,30 @@ const Navbar = () => {
     const navRef = useRef(null);
 
     const showNavbar = () => {
-      setShowNav(!showNav)
-    } 
-
-    useEffect(() => {
-       if(!showNav) {
-         navRef.current.style.display = "hidden"
-        }
-
-        navRef.current.style.visibility = "visible"
-        console.log('deleted')
-
-    }, [showNav])
-
+        setShowNav(!showNav)
+    }
+    
   return (
     <Fragment>
 
       <CgDetailsMore 
           onClick={showNavbar} 
-          className="text-5xl relative md:hidden sm:hidden top-6 text-white z-20 left-10 cursor-pointer" 
+          className={`${styles.navToggle} text-5xl relative top-6 text-white z-20 left-10 cursor-pointer`} 
         />
 
-      <nav ref={navRef} className={`${styles.nav} transition-all duration-500 ${ showNav ? "hideNav" : 'showNav' } flex justify-between items-center relative left-20 right-20 top-16 h-6 z-10`}>
-          <h2 className={`${styles.navTitle}`}> MNTN </h2>
-          <ul className="grid md:grid-cols-3 grid-cols-1 gap-9 text-lg font-bold leading-5">
-              <Link href="#" passHref>
-                <li> Equipment </li>
-              </Link>
-              <Link href="#" passHref>
-                <li> About Us </li>
-              </Link>
-              <Link href="#" passHref>
-                <li> Blog </li>
-              </Link>
+      <nav className={`${styles.nav} ${ showNav ? styles.showNav : styles.hideNav } `}>
+          <h2 className={styles.navTitle}> MNTN </h2>
+          <ul className={styles.navLinks}>
+             <li className="text-lg leading-6">About</li>
+             <li className="text-lg leading-6">Equipment</li>
+             <li className="text-lg leading-6">Blog</li>
           </ul>
-          <div className="flex gap-3 items-center md:mr-20 text-lg leading-6">
-              <VscAccount />
-              <h3> Account </h3>
+          <div className={`${styles.navRight} text-lg leading-5`}>
+             <VscAccount />
+             <h3> Account </h3>
           </div>
       </nav>
+      
     </Fragment>
   )
 }
